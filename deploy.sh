f09ae7bb4b9820d0a1e9e90abfb4552bba6e8e71
NoT1me <ntnosteam@gmail.com>
2020-02-18 05:04:41 +0300

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t not1me/multi-client:latest -t not1me/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t not1me/multi-server:latest -t not1me/multi-server:$SHA -f ./multi-server/Dockerfile ./server
+docker build -t not1me/multi-worker:latest -t not1me/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push not1me/multi-client:latest
+docker push not1me/multi-server:latest
+docker push not1me/multi-worker:latest
+
+docker push not1me/multi-client:$SHA
+docker push not1me/multi-server:$SHA
+docker push not1me/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=not1me/multi-server:$SHA
+kubectl set image deployments/client-deployment client=not1me/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=not1me/multi-worker:$SHA
